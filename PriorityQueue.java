471f740e2a10bf2fcf44dd1255326ab2bed12d02
Ruochong Wu <rwu3@stuy.edu>
2016-05-04 15:28:04 -0400

    Added interface

---
+++ PriorityQueue.java
@@ -0,0 +1,13 @@
+/*
+Team Legendary Spoon - Calvin Vuong, Ruochong Wu, Zicheng Zhen
+APCS2 PD10
+HW35 -- Name / Desc
+2016-05-04
+*/
+
+public interface PriorityQueue{
+    void add(Object x);
+    boolean isEmpty();
+    Object peekMin();
+    Object removeMin();
+}
